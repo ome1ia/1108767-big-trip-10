4e154359ea95f55af47777b84381738b5b57f570
ome1ia <o_melnikova@inbox.ru>
2019-11-29 12:04:05 +0400

    envirinment: webpack etc

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const createMenuTemplate = () => {
   return `<nav class="trip-controls__trip-tabs  trip-tabs">
               <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
