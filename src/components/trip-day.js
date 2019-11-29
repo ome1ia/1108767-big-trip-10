2297ad52bf7f8c0a55e31a8080ca2b2e5b8ad990
ome1ia <o_melnikova@inbox.ru>
2019-11-29 17:42:16 +0400

    delete space in template

--- src/components/trip-day.js
+++ src/components/trip-day.js
@@ -1,5 +1,5 @@
 export const createTripDayTemplate = () => {
-  return `            <li class="trip-days__item  day">
+  return `<li class="trip-days__item  day">
               <div class="day__info">
                 <span class="day__counter">1</span>
                 <time class="day__date" datetime="2019-03-18">MAR 18</time>
