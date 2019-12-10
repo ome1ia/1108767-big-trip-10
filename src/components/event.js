3de73f2a82f3db2c083ffaffbf29cf85b3cd7bfe
ome1ia <o_melnikova@inbox.ru>
2019-12-10 09:28:06 +0400

    task#3: event template + mock

--- src/components/event.js
+++ src/components/event.js
@@ -1,31 +1,104 @@
+import {getEvent} from '../mock/event.js';
+
 export const createEventTemplate = () => {
+  let {type, description, startTime, endTime, price, options} = getEvent();
+
+  const icons = {
+    'trip': `trip`,
+    'transport': `transport`,
+    'train': `train`,
+    'taxi': `taxi`,
+    'sightseeing': `sightseeing`,
+    'ship': `ship`,
+    'restaurant': `restaurant`,
+    'flight': `flight`,
+    'drive': `drive`,
+    'check': `check-in`,
+    'bus': `bus`
+  };
+
+  let icon = icons[type.toLowerCase()];
+
+  const parseTime = (time) => {
+    return /\d{2}:\d{2}/.exec(time.toString());
+  };
+
+  const castTimeFormat = (value) => {
+    return value < 10 ? `0${value}` : String(value);
+  };
+
+  const parseTimeDiff = (diff) => {
+    let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    let hoursDiff = diff - (days * 1000 * 60 * 60 * 24);
+    let hours = Math.floor(hoursDiff / (1000 * 60 * 60));
+    let minutesDiff = hoursDiff - (hours * 1000 * 60 * 60);
+    let minutes = Math.floor(minutesDiff / (1000 * 60));
+
+    if (days) {
+      days = `${castTimeFormat(days)}D `;
+    } else {
+      days = ``;
+    }
+
+    hours = `${castTimeFormat(hours)}H `;
+    minutes = `${castTimeFormat(minutes)}M`;
+
+    return `${days}${hours}${minutes}`;
+  };
+
+  console.log(startTime);
+  console.log(endTime);
+
+  let startTimeFormatted = parseTime(startTime);
+  let endTimeFormatted = parseTime(endTime);
+  let timeDiff = parseTimeDiff(endTime - startTime);
+
+  const setOptions = (items) => {
+    let template = ``;
+
+    for (let i = 0; i < items.length; i++) {
+      let offerTitle = items[i].title;
+      let offerPrice = items[i].price;
+
+      template += `<li class="event__offer">
+                <span class="event__offer-title">${offerTitle}</span>
+                &plus;
+                &euro;&nbsp;<span class="event__offer-price">${offerPrice}</span>
+               </li>`;
+    }
+
+    if (items.length) {
+      template = `<h4 class="visually-hidden">Offers:</h4>
+            <ul class="event__selected-offers">
+              ${template}
+            </ul>`;
+    }
+
+    return template;
+  };
+
+  let optionsParsed = setOptions(options);
+
   return `<div class="event">
             <div class="event__type">
-              <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+              <img class="event__type-icon" width="42" height="42" src="img/icons/${icon}.png" alt="${type}">
             </div>
-            <h3 class="event__title">Taxi to airport</h3>
+            <h3 class="event__title">${description}</h3>
 
             <div class="event__schedule">
               <p class="event__time">
-                <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+                <time class="event__start-time" datetime="${startTime}">${startTimeFormatted}</time>
                 &mdash;
-                <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+                <time class="event__end-time" datetime="${endTime}">${endTimeFormatted}</time>
               </p>
-              <p class="event__duration">1H 30M</p>
+              <p class="event__duration">${timeDiff}</p>
             </div>
 
             <p class="event__price">
-              &euro;&nbsp;<span class="event__price-value">20</span>
+              &euro;&nbsp;<span class="event__price-value">${price}</span>
             </p>
 
-            <h4 class="visually-hidden">Offers:</h4>
-            <ul class="event__selected-offers">
-              <li class="event__offer">
-                <span class="event__offer-title">Order Uber</span>
-                &plus;
-                &euro;&nbsp;<span class="event__offer-price">20</span>
-               </li>
-            </ul>
+            ${optionsParsed}
 
             <button class="event__rollup-btn" type="button">
               <span class="visually-hidden">Open event</span>
---
+++ src/mock/event.js
@@ -0,0 +1,136 @@
+const getRandom = (max, min = 0) => {
+
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+
+};
+
+const getEventType = () => {
+  const eventTypes = [
+    `taxi`,
+    `bus`,
+    `train`,
+    `ship`,
+    `transport`,
+    `drive`,
+    `flight`,
+    `check`,
+    `sightseeing`,
+    `restaurant`];
+
+  return eventTypes[getRandom(eventTypes.length - 1)];
+};
+
+const getCity = () => {
+  const cities = [`Amsterdam`, `Barselona`, `Madrid`, `Rome`];
+
+  return cities[getRandom(cities.length - 1)];
+};
+
+const getPhotoes = () => {
+  const photoesSize = getRandom(6);
+  let photoes = [];
+
+  for (let i = 0; i < photoesSize; i++) {
+    photoes.push(`http://picsum.photos/300/150?r=${Math.random()}`);
+  }
+
+  return photoes;
+};
+
+const getDescription = () => {
+  const lipsum = `Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit. 
+    Cras aliquet varius magna, non porta ligula feugiat eget. 
+    Fusce tristique felis at fermentum pharetra. 
+    Aliquam id orci ut lectus varius viverra. 
+    Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. 
+    Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. 
+    Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, 
+    eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis 
+    suscipit in sed felis. Aliquam erat volutpat. 
+    Nunc fermentum tortor ac porta dapibus. 
+    In rutrum ac purus sit amet tempus.`.split(`. `);
+
+  let descriptionSize = getRandom(1, 3);
+  let description = ``;
+
+  for (let i = 0; i < descriptionSize; i++) {
+    description += lipsum[getRandom(lipsum.length - 1)];
+  }
+
+  return description;
+};
+
+const getDate = () => {
+  let time = new Date().getTime();
+  let dateDiff = getRandom(10) * 4 * 60 * 60 * 1000;
+
+  time += dateDiff;
+  let day = new Date(time);
+  let timeDiff = getRandom(12 * 60 * 60 * 1000); // пусть разница будет в пределах 24 часов
+  let startTime = new Date(time - timeDiff);
+  let endTime = new Date(time + timeDiff);
+
+  return {
+    day,
+    startTime,
+    endTime
+  };
+};
+
+const getPrice = () => {
+  return getRandom(200);
+};
+
+const getOptions = () => {
+  const optonDescription = [
+    `Order Uber`,
+    `Add luggage`,
+    `Switch to comfort`,
+    `Rent a car`,
+    `Add breakfast`,
+    `Book tickets`,
+    `Lunch in city`
+  ];
+
+  let optionsSize = getRandom(2);
+  let options = [];
+
+  for (let i = 0; i < optionsSize; i++) {
+    options.push({
+      type: `option`, // а какой тип может быть? В ТЗ написано "дополнительные опции — это структура данных с типом, названием и ценой."
+      title: optonDescription[getRandom(optonDescription.length - 1)],
+      price: getRandom(200)
+    });
+  }
+
+  return options;
+};
+
+const getEvent = () => {
+  let {day, startTime, endTime} = getDate();
+
+  return {
+    type: getEventType(),
+    city: getCity(),
+    photoes: getPhotoes(),
+    description: getDescription(),
+    day,
+    startTime,
+    endTime,
+    price: getPrice(),
+    options: getOptions()
+  };
+};
+
+const getEventsList = (size) => {
+  let eventList = [];
+
+  for (let i = 0; i < size; i++) {
+    let event = getEvent();
+    eventList.push(event);
+  }
+};
+
+export {getEvent, getEventsList};
