721090110ec12c58f10cd3984360fd4c1f1186cb
ome1ia <o_melnikova@inbox.ru>
2020-01-10 10:31:46 +0400

    module#6: fix abstract-smart-component eslint

--- src/components/abstract-smart-component.js
+++ src/components/abstract-smart-component.js
@@ -1,12 +1,11 @@
 import AbstractComponent from './abstract-component.js';
-import {render} from '../utils/render.js';
 
 export default class AbstractSmartComponent extends AbstractComponent {
   recoveryListeners() {
     throw new Error(`Abstract method not implemented: recoveryListeners`);
   }
 
-  rerender({id, type, city, photoes, description, isFavorite, startTime, endTime, price, options}) {
+  rerender() {
     const oldElement = this.getElement();
     this._element = null;
     const newElement = this.getElement();
